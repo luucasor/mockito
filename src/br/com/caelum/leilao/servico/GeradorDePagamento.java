d1fb75b45882002de2a22e95dbea491ed2e1cd89
luucasor <lucas@gati.net.br>
2017-10-17 22:49:28 -0200

    Teste de funcionalidade que pula o pagamento para o proximo dia util

--- src/br/com/caelum/leilao/servico/GeradorDePagamento.java
+++ src/br/com/caelum/leilao/servico/GeradorDePagamento.java
@@ -12,13 +12,22 @@ public class GeradorDePagamento {
     private final RepositorioDePagamentos pagamentos;
     private final RepositorioDeLeiloes leiloes;
     private final Avaliador avaliador;
+    private final Relogio relogio;
 
     public GeradorDePagamento(RepositorioDeLeiloes leiloes,
                               RepositorioDePagamentos pagamentos,
-                              Avaliador avaliador) {
+                              Avaliador avaliador, Relogio relogio) {
         this.leiloes = leiloes;
         this.pagamentos = pagamentos;
         this.avaliador = avaliador;
+        this.relogio = relogio;
+    }
+
+    public GeradorDePagamento(RepositorioDeLeiloes leiloes,
+                              RepositorioDePagamentos pagamentos,
+                              Avaliador avaliador){
+        this(leiloes, pagamentos, avaliador, new RelogioDoSistema());
+
     }
 
     public void gera() {
@@ -28,8 +37,20 @@ public class GeradorDePagamento {
             avaliador.avalia(leilao);
 
             Pagamento novoPagamento =
-                    new Pagamento(avaliador.getMaiorLance(), Calendar.getInstance());
+                    new Pagamento(avaliador.getMaiorLance(), primeiroDiaUtil());
             pagamentos.salva(novoPagamento);
         }
+    }
+
+    private Calendar primeiroDiaUtil(){
+        Calendar data = relogio.hoje();
+        int diaDaSemana = data.get(Calendar.DAY_OF_WEEK);
+
+        if(diaDaSemana == Calendar.SATURDAY){
+            data.add(Calendar.DAY_OF_WEEK, 2);
+        } else if (diaDaSemana == Calendar.SUNDAY){
+            data.add(Calendar.DAY_OF_WEEK, 1);
+        }
+        return data;
     }
 }
--- test/br/com/caelum/leilao/servico/GeradorDePagamentoTest.java
+++ test/br/com/caelum/leilao/servico/GeradorDePagamentoTest.java
@@ -7,11 +7,14 @@ import br.com.caelum.leilao.dominio.Usuario;
 import br.com.caelum.leilao.infra.dao.RepositorioDeLeiloes;
 import br.com.caelum.leilao.infra.dao.RepositorioDePagamentos;
 import static org.junit.Assert.*;
+
+import com.sun.org.apache.regexp.internal.RE;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import sun.security.x509.AVA;
 
 import java.util.Arrays;
+import java.util.Calendar;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -38,5 +41,36 @@ public class GeradorDePagamentoTest {
         verify(pagamentos).salva(argumento.capture());
         Pagamento pagamentoGerado = argumento.getValue();
         assertEquals(2500.0, pagamentoGerado.getValor(), 0.00001);
+    }
+
+    @Test
+    public void deveEmpurrarParaOProximoDiaUtil(){
+        RepositorioDeLeiloes leiloes = mock(RepositorioDeLeiloes.class);
+        RepositorioDePagamentos pagamentos = mock(RepositorioDePagamentos.class);
+        Relogio relogio = mock(Relogio.class);
+
+        Calendar sabado = Calendar.getInstance();
+        sabado.set(2012, Calendar.APRIL, 7);
+
+        when(relogio.hoje()).thenReturn(sabado);
+
+        Leilao leilao = new CriadorDeLeilao()
+                .para("Playstation")
+                .lance(new Usuario("José"), 2000.0)
+                .lance(new Usuario("Maria"), 2500.0)
+                .constroi();
+
+        when(leiloes.encerrados()).thenReturn(Arrays.asList(leilao));
+
+        GeradorDePagamento gerador = new GeradorDePagamento(leiloes, pagamentos, new Avaliador(), relogio);
+        gerador.gera();
+
+        ArgumentCaptor<Pagamento> argumento = ArgumentCaptor.forClass(Pagamento.class);
+        verify(pagamentos).salva(argumento.capture());
+        Pagamento pagamentoGerado = argumento.getValue();
+
+        int diaDaSemana = pagamentoGerado.getData().get(Calendar.DAY_OF_WEEK);
+        assertEquals(Calendar.MONDAY, diaDaSemana);
+        assertEquals(9, pagamentoGerado.getData().get(Calendar.DAY_OF_MONTH));
     }
 }
